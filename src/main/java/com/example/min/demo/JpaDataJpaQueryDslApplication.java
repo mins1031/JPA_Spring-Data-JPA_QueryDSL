8b337a2a4d87376a9325ab8cb97969055dc7ed2e
minyoung <urisegea@naver.com>
2021-07-01 14:06:46 +0900

    jpa depp study first commit

---
+++ src/main/java/com/example/min/demo/JpaDataJpaQueryDslApplication.java
@@ -0,0 +1,13 @@
+package com.example.min.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDataJpaQueryDslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaDataJpaQueryDslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/min/demo/JpaDataJpaQueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.min.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDataJpaQueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
